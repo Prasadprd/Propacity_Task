a6839cfb44166a5abd5ccdce185d70ceb5ba3d18
Prasad Chaudhari <prasadchaudhari117@gmail.com>
2021-06-16 23:59:14 +0530

    started mobile version

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,6 +4,8 @@ import call from "../../assets/Icon zocial-call.svg"
 import {Button ,Modal} from "react-bootstrap"
 import "./header.css"
 import EnquireModal from "../Modal/enquire-modal"
+import {VscThreeBars} from "react-icons/vsc"
+
 const Header =()=>{
     const [show ,setShow]= useState(false)
 
@@ -16,11 +18,15 @@ const Header =()=>{
     const links =["Home","Resedenties" , "Ameneties" , "Payment plans" , "Floor plans"]
     return (
         <div className="container-fluid" style={{backgroundColor:"FFFFFF",boxShadow: "0px 5px 8px #00000040"}}>
-            <div className="row">
-                <div className="col-12 col-md-2">
-                    <img src={logo} alt="logo" style={{padding:"5px",margin:"5px", height:"100px"}} />
+            <div className="row" style={{justifyContent:"space-evenly"}}>
+                <div className="col-2 d-md-none" style={{padding:"2% 0%" ,margin:"2% 0%",textAlignLast:"center"}}>
+                    <VscThreeBars  />
                 </div>
-                <div className=" col-12 col-md-6">
+                <div className="col-8 col-md-2">
+                    <img className="d-none d-md-inline" src={logo} alt="logo" style={{padding:"2%",margin:"2%", height:"fit-content" ,width:"15vw"}} />
+                    <img className="d-inline d-md-none" src={logo} alt="logo" style={{padding:"2%",margin:"2%", height:"5vh" ,width:"45vw"}} />
+                </div>
+                <div className="d-none d-md-inline-block col-md-6">
                     <div className="row" style={{verticalAlign:"middle" ,height:"100%"}}>
                     {
                         links.map(link=>{
@@ -34,15 +40,15 @@ const Header =()=>{
                     </div>
                     
                 </div>
-                <div className="col"></div>
-                <div className="col-sm-3">
+                <div className="col-2 col-md-3" style={{textAlignLast:"center"}}>
                     <div className="row" style={{height:"100%"}}>
-                        <div className="col contact-info" style={{font:"2.5vh poppins", color:"#BA8C5F" ,fontWeight:"bolder",display:"flex"}} >
-                            <img src={call} alt="contact" />
-                            +91 8999874382
+                        <div className="col contact-info" style={{font:"2.5vh poppins", color:"#BA8C5F" ,fontWeight:"bolder",display:"flex",marginLeft:"auto",marginRight:"auto"}} >
+                            <img  src={call} alt="contact" />
+                            <p className="d-none d-md-inline" style={{marginBottom:"0%"}} >+918999874382</p>
+                            
                         </div>
                         <div className="col contact-info" >
-                            <Button variant="dark" style={{boxShadow: "3px 3px 10px #00000029",
+                            <Button variant="dark" className="d-none d-md-inline" style={{boxShadow: "3px 3px 10px #00000029",
                                     border: "1px solid #2C2E40",font:"poppins",
                                     borderRadius: "10px"}} onClick= {handleShow}>
                                 
